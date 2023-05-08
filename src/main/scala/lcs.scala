4925c4fd15e3e32e25cb98b3f0cf905796c8927f
djbpitt <djbpitt@gmail.com>
2023-05-08 15:00:52 -0400

    Add example with two sequences of strings

--- src/main/scala/lcs.scala
+++ src/main/scala/lcs.scala
@@ -4,7 +4,7 @@
  * Example: (0, 1, 2, 3, 4, 5) and (0, 4, 2, 3, 1, 5) => (0, 2, 3, 5)
  *
  * Based on: https://rosettacode.org/wiki/Longest_common_subsequence#Scala
- * Modified to accept varargs input
+ * Modified to accept more than two inputs
  *
  * TODO: foldLeft() initializes with first value and then folds in all
  *  values, including (redundantly) first. This is the implementation
@@ -12,7 +12,7 @@
  *  In addition to redundancy, requires defining listified separately, instead of single pipeline.
  *  Can we initialize with head and then fold in just tail?
  */
-def lcsLazy[T](inputs: IndexedSeq[T]*): IndexedSeq[T] =
+def lcsLazy[T](inputs: Vector[IndexedSeq[T]]): IndexedSeq[T] =
   val listified =
     inputs
       .map(subsets(_).to(LazyList))
@@ -34,5 +34,9 @@ def subsets[T](u: IndexedSeq[T]): Iterator[IndexedSeq[T]] = {
   val x = Vector(0, 1, 2, 3, 4, 5)
   val y = Vector(0, 4, 2, 3, 1, 5)
   val z = Vector(0, 1, 2, 3, 4, 5)
-  val result = lcsLazy(u, v, w, x, y, z)
-  println(result)
+  val resultInt = lcsLazy(Vector(u, v, w, x, y, z))
+  println(resultInt)
+  val a = Vector("a", "b", "c", "d", "e", "f")
+  val b = Vector("a", "e", "c", "d", "b", "f")
+  val resultStr = lcsLazy(Vector(a, b))
+  println(resultStr)
